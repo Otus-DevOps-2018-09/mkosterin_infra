f611dca79daebd7640c78b40f0a5225ce9b3cac4
Maxim Kosterin <maxim.kosterin@gmail.com>
2018-10-29 21:54:52 +0500

    HW №7

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -5,6 +5,9 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongo-org-3.2.list
 apt update
 apt install -y mongodb-org
+cp -f ~/mongod.conf /etc/
+chown root:root /etc/mongod.conf
+chmod 644 /etc/mongod.conf
 systemctl enable mongod
 echo "Setup MongoDB complete"
 
